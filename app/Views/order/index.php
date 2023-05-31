0867de4ebfaad4054e0d69efc3060201729b6b21
Yasith Chandula <76646376+yasithchandula@users.noreply.github.com>
2023-05-31 12:21:31 +0530

    index_fix

--- app/Views/order/index.php
+++ app/Views/order/index.php
@@ -7,7 +7,7 @@
         <div class="row">
             
             <div class="col md-12">
-                
+
                 <div class="card">
 
                     <div class="card-body">
@@ -68,112 +68,112 @@
          * - Load preapproved orders from db
          */
 
-        // $(document).ready(function(){
-        //     preapprovedOrders();
-        // });
+        $(document).ready(function(){
+            preapprovedOrders();
+        });
 
 
         /**
          * - Listning to click on edit button
          * - Load the Course edit modal and pass the parameters to input fields on the modal
          */
-        // $(document).on('click','.get-payment',function(){
-        //     var order_id = $(this).closest('tr').find('.order_id').text();
-        //     console.log(order_id);
-        //     $.ajax({
-        //         method:"POST",
-        //         url:"payhere_charging",
-        //         data : {
-        //             "order_id":order_id
-        //         },
-        //         success:function(response){
-        //             // console.log(response);
-        //             $.each(response,function(key,value){
-        //                 console.log(key,value);
-        //                 $('.hidden_pre_pay').append('\
-        //                 <input type="hidden" name="'+key+'" id="pr_'+key+'" value='+value+'>');
-        //                 $('.prePayment').modal('show')
-        //             })
+        $(document).on('click','.get-payment',function(){
+            var order_id = $(this).closest('tr').find('.order_id').text();
+            console.log(order_id);
+            $.ajax({
+                method:"POST",
+                url:"payhere_charging",
+                data : {
+                    "order_id":order_id
+                },
+                success:function(response){
+                    // console.log(response);
+                    $.each(response,function(key,value){
+                        console.log(key,value);
+                        $('.hidden_pre_pay').append('\
+                        <input type="hidden" name="'+key+'" id="pr_'+key+'" value='+value+'>');
+                        $('.prePayment').modal('show')
+                    })
 
-        //             }
-        //     })
+                    }
+            })
 
-        // })
+        })
 
         /**
          * - update the course details
          * - Sends the values stored in input fields of the model
          */
 
-        // $(document).on('click','.toPrePaybtn',function(){
+        $(document).on('click','.toPrePaybtn',function(){
 
-        //             var data ={
-        //                 "type": "PAYMENT",
-        //                 "order_id": $('#pr_order_id').val(),
-        //                 "items": 'Course Fee',
-        //                 "currency": "LKR",
-        //                 "amount": $('#pr_amount').val(),
-        //                 "customer_token": $('#pr_customer_token').val(),
-        //                 "custom_1": null,
-        //                 "custom_2": null,
-        //                 "notify_url": "",
-        //                 "itemList":[$('#pr_order_title').val()],
-        //             }
+                    var data ={
+                        "type": "PAYMENT",
+                        "order_id": $('#pr_order_id').val(),
+                        "items": 'Course Fee',
+                        "currency": "LKR",
+                        "amount": $('#pr_amount').val(),
+                        "customer_token": $('#pr_customer_token').val(),
+                        "custom_1": null,
+                        "custom_2": null,
+                        "notify_url": "",
+                        "itemList":[$('#pr_order_title').val()],
+                    }
 
-        //             var auth= 'Bearer '+$('#pr_Authorization').val();
-        //             console.log(auth);
-        //             console.log(data);
+                    var auth= 'Bearer '+$('#pr_Authorization').val();
+                    console.log(auth);
+                    console.log(data);
 
-        //             $.ajax({
-        //                 method:"POST",
-        //                 url:"https://sandbox.payhere.lk/merchant/v1/payment/charge",
-        //                 headers:{
-        //                     'Authorization':auth,
-        //                     'Content-Type': 'application/json'
-        //                 },
-        //                 data:[{'grant_type': 'client_credentials'}],
-        //                 success:function(response){
-        //                     $('#courseModal').modal('hide');
-        //                     $('#courseModal').find('input').val('');
-        //                             if(response.status){
-        //                                 console.log(response);
-        //                             }
-        //                             else{
-        //                                 alertify.error(response.error);
-        //                             }
-        //                 }
-        //             })
+                    $.ajax({
+                        method:"POST",
+                        url:"https://sandbox.payhere.lk/merchant/v1/payment/charge",
+                        headers:{
+                            'Authorization':auth,
+                            'Content-Type': 'application/json'
+                        },
+                        data:[{'grant_type': 'client_credentials'}],
+                        success:function(response){
+                            $('#courseModal').modal('hide');
+                            $('#courseModal').find('input').val('');
+                                    if(response.status){
+                                        console.log(response);
+                                    }
+                                    else{
+                                        alertify.error(response.error);
+                                    }
+                        }
+                    })
             
 
-        // })
+        })
 
 
     /**
      * Load all the course records from the database
      */
 
-        // function preapprovedOrders(){
-        //     $.ajax({
-        //         method:"GET",
-        //         url:"orders/preorders",
-        //         success:function(response){
-        //             console.log(response.order);
-        //             $.each(response.order,function(key,value){
-        //                 console.log(key,value);
-        //                 $('.orderdata').append('<tr>\
-        //                 <td class="order_id">'+value['order_id']+'</td>\
-        //                 <td>'+value['sID']+'</td>\
-        //                 <td>'+value['order_title']+'</td>\
-        //                 <td>'+value['amount']+'</td>\
-        //                 <td>'+value['status_message']+'</td>\
-        //                 <td> <a href="#" class="badge btn btn-primary get-payment">Get Payment</a>\
-        //                 </td>\
-        //                 </tr>');
+        function preapprovedOrders(){
+            $.ajax({
+                method:"GET",
+                url:"orders/preorders",
+                success:function(response){
+                    console.log(response.order);
+                    $.each(response.order,function(key,value){
+                        console.log(key,value);
+                        $('.orderdata').append('<tr>\
+                        <td class="order_id">'+value['order_id']+'</td>\
+                        <td>'+value['sID']+'</td>\
+                        <td>'+value['order_title']+'</td>\
+                        <td>'+value['amount']+'</td>\
+                        <td>'+value['status_message']+'</td>\
+                        <td> <a href="#" class="badge btn btn-primary get-payment">Get Payment</a>\
+                        </td>\
+                        </tr>');
                         
-        //             })
-        //         }
-        //     })
-        // }
+                    })
+                }
+            })
+        }
 
 
     </script>
