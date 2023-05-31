cd4974a5d62c3f8ba4a7129eb740337dc3af5b8c
Yasith Chandula <76646376+yasithchandula@users.noreply.github.com>
2023-05-31 12:31:00 +0530

    routes update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -79,10 +79,10 @@ $routes->get('usercheck','LoginController::checkSession');
   * Order routes
   */
 
-  $routes->get('order/preorders','OrderController::fetchPreOrders',['filter'=>'auth']);
+  $routes->get('orders/preorders','OrderController::fetchPreOrders',['filter'=>'auth']);
   $routes->get('order','OrderController::index',['filter'=>'auth']);
-  $routes->get('order/usercheck','LoginController::checkSession',['filter'=>'auth']);
-  $routes->post('payhere_charging','OrderController::payhereCharging',['filter'=>'auth']);
+  $routes->get('orders/usercheck','LoginController::checkSession',['filter'=>'auth']);
+  $routes->post('orders/payhere_charging','OrderController::payhereCharging',['filter'=>'auth']);
 
 
 
